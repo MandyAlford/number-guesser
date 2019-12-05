999f58249d315f1cbade3124076071e1ed3ff3f7
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-04 17:41:36 -0700

    Create functionality to enable/disable clear form btn

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
 
- var submitBtn = document.querySelector('#submit-guess-btn');
- var submitGuessForm= document.querySelector('#submit-guess-form');
-
+var submitBtn = document.querySelector('#submit-guess-btn');
+var submitGuessForm= document.querySelector('#submit-guess-form');
+var clearFromBtn = document.querySelector('#clear-form-btn');
+var challenger1Name = document.querySelector('#challenger-1-name-input');
+var challenger1Guess = document.querySelector('#challenger-1-guess-input');
+var challenger2Name = document.querySelector('#challenger-2-name-input');
+var challenger2Guess = document.querySelector('#challenger-2-guess-input');
 
 submitGuessForm.addEventListener('input', checkGuessInputs);
+submitGuessForm.addEventListener('input', enableClearBtn)
 
 function checkGuessInputs(){
-  var challenger1Name = document.querySelector('#challenger-1-name-input');
-  var challenger1Guess = document.querySelector('#challenger-1-guess-input');
-  var challenger2Name = document.querySelector('#challenger-2-name-input');
-  var challenger2Guess = document.querySelector('#challenger-2-guess-input');
 
   if (challenger1Name.value && challenger1Guess.value && challenger2Name.value && challenger2Guess.value){
     submitBtn.disabled === false;
   } else {
     submitBtn.disabled=== true;
+  }
+}
+
+function enableClearBtn() {
+  if (challenger1Name.value || challenger1Guess.value || challenger2Name.value || challenger2Guess.value){
+    submitBtn.disabled === false;
+    console.log('clearn btn enabled');
+  } else {
+    submitBtn.disabled=== true;
+    console.log('clearn btn disabled');
   }
 }
