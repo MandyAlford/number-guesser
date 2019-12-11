c6275d22fd738c747c749886605fb69ed57b011c
Tanisha L Davey <tanisha.davey2@gmail.com>
2019-12-10 18:30:55 -0700

    Add functionality to clear all btn to remove all winner's cards from right section

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ var guessCounter= 0;
 var rightSection= document.querySelector('.right-section');
 var errorMessage= document.querySelector('#warning');
 var setRangeForm= document.querySelector('#set-range-inputs');
+var clearAllBtn = document.querySelector('.clear-all-btn');
 
 submitGuessForm.addEventListener('input', checkGuessInputs);
 submitGuessForm.addEventListener('input', enableClearBtn);
@@ -153,8 +154,17 @@ function displayWinnerCard(){
 }
 
 function showClearAllBtn() {
-  var clearAllBtn = document.querySelector('.clear-all-btn');
+  var rightSection = document.querySelector('.right-section')
   clearAllBtn.classList.remove('hidden');
+  rightSection.addEventListener('click', clearAllWinnerCards);
+}
+
+function clearAllWinnerCards() {
+  var winnerCard = document.querySelectorAll('.winner-card');
+  if (event.target.classList.contains('clear-all-btn')) {
+    event.target.nextElementSibling.remove();
+    clearAllBtn.classList.add('hidden');
+  }
 }
 
 function clearGuessCounter(){
